2a4a298ebe73a812832f28803f07ba920b09a24b
George Miller <133229709+gmm2204@users.noreply.github.com>
2023-08-02 14:54:31 +0300

    Added and modified files (#17)

--- src/models/screeningByAgeGender.model.ts
+++ src/models/screeningByAgeGender.model.ts
@@ -1,7 +1,8 @@
 import { Model, DataTypes, Sequelize } from 'sequelize';
 
-export class ScreeningByGender extends Model {
+export class ScreeningByAgeGender extends Model {
 
      public FemaleNumber?: number;
      public MaleNumber?: number;
+     public Age?: number;
 }
--- src/repositories/screening.repository.ts
+++ src/repositories/screening.repository.ts
@@ -1,33 +1,48 @@
 import { QueryTypes } from "sequelize";
 import Database from "../db";
 import { ScreeningByGender } from "../models/screeningByGender.model";
+import { ScreeningByAgeGender } from "../models/screeningByAgeGender.model";
 
 interface IScreeningRepository {
-
-     retrieveScreeningByGender(): Promise<ScreeningByGender[]>
-
+  retrieveScreeningByGender(): Promise<ScreeningByGender[]>;
+  retrieveScreeningByAgeGender(): Promise<ScreeningByAgeGender[]>;
 }
 
 class ScreenigRepository implements IScreeningRepository {
 
-    db = new Database();
-    private retrievedScreeningData :any;
+  db = new Database();
+  private retrievedScreeningData: any;
+  private retrievedScreeningByAgeGender: any;
 
-    async retrieveScreeningByGender(): Promise<ScreeningByGender[]> {
-       const query = `SELECT 
+  async retrieveScreeningByGender(): Promise<ScreeningByGender[]> {
+    const query = `SELECT 
            count(Screened) as Screened,
           (Select sexValue from [dbo].[DimSex] where SexID = SEX) as Gender  
             FROM [dbo].[FactMortality]
             WHERE Screened = 1  
              GROUP BY Sex`
-            this.retrievedScreeningData = await this.db.sequelize?.query<ScreeningByGender[]>(query, {
-            type: QueryTypes.SELECT,
+    this.retrievedScreeningData = await this.db.sequelize?.query<ScreeningByGender[]>(query, {
+      type: QueryTypes.SELECT,
+    });
 
-             });
+    console.log(this.retrievedScreeningData);
+    return this.retrievedScreeningData;
+  }
 
-              console.log(this.retrievedScreeningData);
-            return this.retrievedScreeningData;
-    }
+  async retrieveScreeningByAgeGender(): Promise<ScreeningByAgeGender[]> {
+    const query = `SELECT 
+           count(Screened) as Screened,
+          (Select sexValue from [dbo].[DimSex] where SexID = SEX) as Gender  
+            FROM [dbo].[FactMortality]
+            WHERE Screened = 1  
+             GROUP BY Sex`
+    this.retrievedScreeningByAgeGender = await this.db.sequelize?.query<ScreeningByAgeGender[]>(query, {
+      type: QueryTypes.SELECT,
+    });
+
+    console.log(this.retrievedScreeningByAgeGender);
+    return this.retrievedScreeningByAgeGender;
+  }
 
 }
 export default new ScreenigRepository
