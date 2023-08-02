9f81eadfdc5845673bf318488c0dc51513652db6
unknown <kamau_amos@live.com>
2023-08-02 15:47:49 +0300

    Added retrieval of Screened by Health Facilities data from database

--- src/repositories/screening.repository.ts
+++ src/repositories/screening.repository.ts
@@ -2,10 +2,12 @@ import { QueryTypes } from "sequelize";
 import Database from "../db";
 import { ScreeningByGender } from "../models/screeningByGender.model";
 import { ScreeningByAgeGender } from "../models/screeningByAgeGender.model";
+import { numberScreenedHealthFacilities } from "../models/numberScreenedHealthFacilities.model";
 
 interface IScreeningRepository {
   retrieveScreeningByGender(): Promise<ScreeningByGender[]>;
   retrieveScreeningByAgeGender(): Promise<ScreeningByAgeGender[]>;
+  retrieveScreeningByHealthFacilities(): Promise<numberScreenedHealthFacilities[]>;
 }
 
 class ScreeningRepository implements IScreeningRepository {
@@ -13,6 +15,7 @@ class ScreeningRepository implements IScreeningRepository {
   db = new Database();
   private retrievedScreeningData: any;
   private retrievedScreeningByAgeGender: any;
+  private retrievedScreeningByHealthFacilities: any;
 
   async retrieveScreeningByGender(): Promise<ScreeningByGender[]> {
     const query = `SELECT 
@@ -20,7 +23,7 @@ class ScreeningRepository implements IScreeningRepository {
           (Select sexValue from [dbo].[DimSex] where SexID = SEX) as Gender  
             FROM [dbo].[FactMortality]
             WHERE Screened = 1  
-             GROUP BY Sex`
+             GROUP BY Sex`  //Query for ScreeningByGender Data
     this.retrievedScreeningData = await this.db.sequelize?.query<ScreeningByGender[]>(query, {
       type: QueryTypes.SELECT,
     });
@@ -35,7 +38,7 @@ class ScreeningRepository implements IScreeningRepository {
           (Select sexValue from [dbo].[DimSex] where SexID = SEX) as Gender  
             FROM [dbo].[FactMortality]
             WHERE Screened = 1  
-             GROUP BY Sex`
+             GROUP BY Sex` //Query for ScreeningByAgeGender Data
     this.retrievedScreeningByAgeGender = await this.db.sequelize?.query<ScreeningByAgeGender[]>(query, {
       type: QueryTypes.SELECT,
     });
@@ -44,5 +47,22 @@ class ScreeningRepository implements IScreeningRepository {
     return this.retrievedScreeningByAgeGender;
   }
 
+  async retrieveScreeningByHealthFacilities(): Promise<numberScreenedHealthFacilities[]> {
+    const query = `SELECT 
+           count(Screened) as Screened,
+          (Select sexValue from [dbo].[DimSex] where SexID = SEX) as Gender  
+            FROM [dbo].[FactMortality]
+            WHERE Screened = 1  
+             GROUP BY Sex` //Test Query
+    this.retrievedScreeningByHealthFacilities = await this.db.sequelize?.query<numberScreenedHealthFacilities[]>(query, {
+      type: QueryTypes.SELECT,
+    });
+
+    console.log(this.retrievedScreeningByHealthFacilities);
+    return this.retrievedScreeningByHealthFacilities;
+  }
+
 }
+
+
 export default new ScreeningRepository
