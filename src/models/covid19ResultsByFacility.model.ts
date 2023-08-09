f2a6ee05e4f60740b94c46ad8ce71f2798f2b6bb
Amos Kamau <34518963+Kambreezy@users.noreply.github.com>
2023-08-09 09:41:29 +0300

    Merged with new refactored Covid 19 and Enrollment models (#27)
    * merge issues
    
    * Add files to git ignore
    
    * Added config file to git ignore
    
    * Added own server configuration
    
    * Changed some wordings
    
    * Changed the class name
    
    * Added Screening by Health Facilities model
    
    * added screening by Health facility Route function
    
    * Added retrieval of Screened by Health Facilities data from database
    
    * Added Screening by Health Facilities Route
    
    * added screening Over Time
    
    * added screening over time route function
    
    * Added Screening Over Time Route
    
    * Added queries for Screening Over Time
    
    * Added Covid19 Positivity By Gender
    
    * Added Covid19 Overall Positivity By Facility Model,Controller,Route and Repository
    
    * Added Screening models and routes
    
    * Created a Dockerfile, Modified the Screening queries
    
    * Delete db.config.ts
    
    * Modified the routes for Covid19 Results and Enrollment
    
    ---------
    
    Co-authored-by: George Miller <133229709+gmm2204@users.noreply.github.com>

--- src/models/covid19ResultsByFacility.model.ts
+++
@@ -1,7 +0,0 @@
-import { Model, DataTypes, Sequelize } from 'sequelize';
-
-export class Covid19ResultsByFacility extends Model {
-     public Facility?: string;
-     public Covi19Positive?: number;
-     public Covi19Negative?: number;
-}
--- src/models/covid19ResultsByStatus.model.ts
+++
@@ -1,6 +0,0 @@
-import { Model, DataTypes, Sequelize } from 'sequelize';
-
-export class Covid19ResultsByStatus extends Model {
-     public Covi19Positive?: number;
-     public Covi19Negative?: number;
-}
